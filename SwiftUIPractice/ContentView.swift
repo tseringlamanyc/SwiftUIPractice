19d4284189e0b573078108f9111271312faa23d6
Tsering Lama <tseringlama@pursuit.org>
2020-01-07 21:09:19 -0500

    added a dark background and change the text color and the map scheme

--- SwiftUIPractice/ContentView.swift
+++ SwiftUIPractice/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack { // overlapping content 
+            Color.black
+                .edgesIgnoringSafeArea(.all)
         VStack {
             MapView()
-                .frame(height: 300)
+                .frame(height: 400)
             
             StupaImage()
                 .offset(y: -130) // moves image upward
@@ -21,23 +24,27 @@ struct ContentView: View {
             VStack(alignment: .leading) { // aligns by leading edges
                 Text("Boudha Stupa")
                     .font(.title)
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                 HStack {
                     Text("Kathmandu")
                         .font(.subheadline)
+                        .foregroundColor(.white)
                     Spacer() // creates space and pushes them to the oppsite side of the screen
                     Text("Nepal")
                         .font(.subheadline)
+                        .foregroundColor(.white)
                 }
             }
             .padding() // gives label more space so its not sticking to the edges of screen
             Spacer() // pushes it up
         }
     }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .colorScheme(.dark)
     }
 }
